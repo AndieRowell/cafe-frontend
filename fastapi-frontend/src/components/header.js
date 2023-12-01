db03b01c39ef626efc299ce85f79eb8a738d00aa
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-12-01 11:43:55 -0500

    got hamburger menu added

--- fastapi-frontend/src/components/header.js
+++ fastapi-frontend/src/components/header.js
@@ -1,7 +1,12 @@
 //_rfce - should work with es7 snippet extension
 
 import Image from "next/image"; //next js image - compresses for you
-import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import {
+  MagnifyingGlassIcon,
+  UserCircleIcon,
+  Bars3Icon,
+  UsersIcon,
+} from "@heroicons/react/24/solid";
 
 function Header() {
   return (
@@ -16,17 +21,26 @@ function Header() {
           objectFit="contain"
           objectPosition="left"
         />
-        
       </div>
 
       {/* middle div */}
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
-        <input className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400" type="text" placeholder="Where to next?" />
+        <input
+          className="flex-grow pl-5 bg-transparent outline-none text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder="Where to next?"
+        />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-green-400 text-white rounded-full stroke-2 p-2 cursor-pointer md:mx-2" />
       </div>
 
       {/* right div */}
-      <div></div>
+      <div className="flex items-center space-x-4 text-gray-500 justify-end ">
+        <p className="hidden md:inline cursor-pointer">Sign Up</p>
+        <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
+          <Bars3Icon className="h-6" />
+          <UserCircleIcon className="h-6" />
+        </div>
+      </div>
     </header>
   );
 }
--- fastapi-frontend/src/components/navbar.js
+++ fastapi-frontend/src/components/navbar.js
@@ -1,21 +1,21 @@
 //! navbar component
 
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return(
-        <> //fragment tag
-            <Navlink to="/">
-                <h1>Logo</h1>
-            </Navlink>
-            <Bars />
-            <NavMenu>
-                <Navlink to="/">
-                    
-                </Navlink>
-            </NavMenu>
-        </>
-    )
+  return (
+    <>
+      {" "}
+      //fragment tag
+      <Navlink to="/">
+        <h1>Logo</h1>
+      </Navlink>
+      <Bars />
+      <NavMenu>
+        <Navlink to="/"></Navlink>
+      </NavMenu>
+    </>
+  );
 };
 
 export default Navbar;
